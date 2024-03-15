9be63a7d4d9f733f67498f34882b775d5cd7353f
= <androvidd380@gmail.com>
2024-03-15 10:16:52 -0600

    new function code whatsapp

--- src/assets/js/generadorQR.js
+++ src/assets/js/generadorQR.js
@@ -1,18 +1,47 @@
+// Importaciones
 import QRCode from "qrcode";
 
+// Controles de QR
 const button = document.querySelector("#btnGenerar");
 const buttonReset = document.querySelector("#btnReset");
-const inpQRURL = document.querySelector("#inpURLQr");
 const responseIMG = document.querySelector("#imgResponseQR");
 const downloadA = document.querySelector("#downloadQR");
 
+// Enlace
+const inpQRURL = document.querySelector("#inpURLQr");
+
+// Whatsapp
+const inpNumber = document.querySelector("#inpNumber");
+const taMsgWhats = document.querySelector("#taMsgWhats");
+
+// Secciones
+const sWhats = document.querySelector("#sWhatsapp");
+const sLink = document.querySelector("#sEnlace");
+
+//Selectores
+const rLink = document.querySelector("#rLink");
+const rWhats = document.querySelector("#rWhatsapp");
+
+// Configuraciones
 const spanSize = document.querySelector("#sizePX");
 const rangeSize = document.querySelector("#sizeQR");
 const sizeUnit = 41;
 let scaleQR = 5;
-
 spanSize.innerHTML = sizeUnit * scaleQR;
 
+// Funciones de Selectores
+const onSelect = (e) => {
+    if(rLink.checked){
+        sLink.classList.remove("hidden");
+        sWhats.classList.add("hidden");
+    }
+    if(rWhats.checked){
+        sLink.classList.add("hidden");
+        sWhats.classList.remove("hidden");
+    }
+}
+
+// Funciones del QR
 const onResize = (e) => {
     const num = e.target.value;
     const resize = sizeUnit * num;
@@ -20,7 +49,7 @@ const onResize = (e) => {
     scaleQR = num;
 }
 
-const generarQRString = (valor) => {
+const generarQR = (valor) => {
 
     try { 
         QRCode.toDataURL(valor, { scale: scaleQR }, (err, url) => {
@@ -41,6 +70,19 @@ const generarQRString = (valor) => {
     }
 }
 
+const generateQROptionSelected = () => {
+    let valor;
+    if(rLink.checked) {
+        valor = inpQRURL.value;
+    }
+    if(rWhats.checked) {
+        const vNumber = inpNumber.value;
+        const vMsg = taMsgWhats.value;
+        valor = `https://api.whatsapp.com/send/?phone=${vNumber}&text=${vMsg}`
+    }
+    generarQR(valor);
+}
+ 
 const resetQR = () => {
     downloadA.classList.add("pointer-events-none", "bg-slate-400");
     buttonReset.classList.add("pointer-events-none", "bg-slate-400", "text-slate-900");
@@ -50,6 +92,11 @@ const resetQR = () => {
     inpQRURL.value = null;
 }
 
+// Inicializadores
+onSelect();
+rLink.addEventListener("input", onSelect);
+rWhats.addEventListener("input", onSelect);
+
 buttonReset.addEventListener("click", resetQR);
-button.addEventListener("click", () => generarQRString(inpQRURL.value));
+button.addEventListener("click", generateQROptionSelected);
 rangeSize.addEventListener("input", (e) => onResize(e));
